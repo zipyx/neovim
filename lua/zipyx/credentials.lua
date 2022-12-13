1775cbd28ceadea038bb605c39f6beee5cd4ac8f
Setephano <setephano@gmail.com>
2022-12-13 17:37:59 +1300

    Adding some new files for vim keys tracking

--- lua/zipyx/credentials.lua
+++ lua/zipyx/credentials.lua
@@ -1,6 +1,6 @@
--- vim.g.calendar_google_api_key = "AIzaSyAr71_ueZSoQIwFpm097txlulum2VLUdnk"
--- vim.g.calendar_google_client_id = "50625262874-2rksh73f417h9801auvtbm5rral1rsif.apps.googleusercontent.com"
--- vim.g.calender_google_client_secret = "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"
+vim.g.calendar_google_api_key = "AIzaSyAr71_ueZSoQIwFpm097txlulum2VLUdnk"
+vim.g.calendar_google_client_id = "50625262874-2rksh73f417h9801auvtbm5rral1rsif.apps.googleusercontent.com"
+vim.g.calender_google_client_secret = "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"
 
 -- vim.g.calendar_google_api_key = "AIzaSyBGSn6EvW8kXdN3S7253Syo15JZP8iAgmQ"
 -- vim.g.calendar_google_client_id = "50625262874-lqcldrhj9qdgd7kqbi6k6l3979o5enim.apps.googleusercontent.com"
