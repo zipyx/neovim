472261a684f3aaee546796e2e74973417ed8b005
Setephano <setephano@gmail.com>
2022-12-18 21:37:21 +1300

    Fix : typo issue

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -1,7 +1,7 @@
 local status, saga = pcall(require, "lspsaga")
 if (not status) then return end
 
-saga.saga.init_lsp_saga {
+saga.init_lsp_saga {
   server_filetype_map = {
     typescript = 'typescript',
     javascript = 'javascript',
