1f8892a8c54a42f8bfc924594b6c4c71a2871368
Setephano <setephano@gmail.com>
2022-12-18 23:38:18 +1300

    Removing setup rquirements in init.lua

--- init.lua
+++ init.lua
@@ -11,39 +11,6 @@ require('zipyx.plugin.keymaps')
 -- require('zipyx.plugins')
 -- require('zipyx.globals')
 
--- require('code_runner').setup({
---   -- put here the commands by filetype
---   filetype = {
---     c = "cd $dir && cc ./$fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
---     cs = "cd $dir && mcs ./$fileName -out:$fileNameWithoutExt.exe && mono ./$fileNameWithoutExt.exe",
---     cpp = "cd $dir && g++ ./$fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
---     lua = "cd $dir && lua ./$fileName",
---     java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
---     python = "python3 -u",
---     javascript = "node",
---     typescript = "deno run",
---     rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt"
---   },
--- })
-
--- require('nvim-tree').setup({
---   sort_by = "case_sensitive",
---   view = {
---     adaptive_size = true,
---     mappings = {
---       list = {
---         { key = "u", action = "dir_up" },
---       },
---     },
---   },
---   renderer = {
---     group_empty = true,
---   },
---   filters = {
---     dotfiles = true,
---   },
--- })
-
 local has = vim.fn.has
 local is_mac = has "macunix"
 local is_win = has "win32"
