402bdc64d613e03abee7de33f2373914a23ea973
Setephano <setephano@gmail.com>
2023-02-22 21:55:07 +1300

    remove: deleted old unsable code due to breaking changes

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -32,31 +32,3 @@ keymap("v", 'g.', function()
   vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-U>", true, false, true))
   codeaction:range_code_action()
 end, { silent = true })
-
--- ##########################################################################
--- OLD CONFIG FILE (Breaking changes with plugin on 2023)
--- ##########################################################################
--- -- DO NOT USE THIS!!!! THIS IS CAUSING THE CONFIG FILE TO BREAK
--- saga.init_lsp_saga {
---   server_filetype_map = {
---     typescript = 'typescript',
---     javascript = 'javascript',
---     json = 'json',
---     lua = 'lua',
---     python = 'python',
---     go = 'go',
---     rust = 'rust',
---     cpp = 'cpp',
---     c = 'c',
---     csharp = 'csharp',
---   }
--- }
-
--- vim.keymap.set('n', '<C-n>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
--- vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
--- vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
--- vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
--- vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
--- vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
--- vim.keymap.set('n', 'g.', '<Cmd>Lspsaga code_action<CR>', opts)
--- vim.keymap.set('n', 'g,', '<Cmd>Lspsaga references<CR>', opts)
