5b98010af94b41bda8abd8f773754b4291e8ce5a
Setephano <setephano@gmail.com>
2023-03-26 22:58:06 +1300

    add: new temp changes to config

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -12,23 +12,23 @@ saga.setup({
 })
 
 local diagnostic = require("lspsaga.diagnostic")
-local keymap = vim.keymap.set
+local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
-keymap('n', '<C-j>', diagnostic.goto_next, opts)
-keymap('n', 'gl', diagnostic.show_diagnostics, opts)
-keymap('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
-keymap('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
--- vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
-keymap('i', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-keymap('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
-keymap('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
-keymap('n', 'g,', '<Cmd>Lspsaga references<CR>', opts)
+-- keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
+keymap.set('n', '<C-n>', diagnostic.goto_next, opts)
+keymap.set('n', '<C-p', diagnostic.goto_prev, opts)
+keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
+keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
+keymap.set('i', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
+keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
+keymap.set('n', 'g,', '<Cmd>Lspsaga references<CR>', opts)
 
 -- Code action
 local codeaction = require("lspsaga.codeaction")
-keymap('n', 'g.', function() codeaction:code_action() end, { silent = true })
-keymap("v", 'g.', function()
+keymap.set('n', 'g.', function() codeaction:code_action() end, { silent = true })
+keymap.set("v", 'g.', function()
   vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-U>", true, false, true))
   codeaction:range_code_action()
 end, { silent = true })
